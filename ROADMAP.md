e0a9ab6b84f03d8a56fd7828f94927cb8aec48a0
ptung <misuke01@gmail.com>
2021-12-14 16:32:45 -0500

    Add street bet

--- ROADMAP.md
+++ ROADMAP.md
@@ -5,6 +5,6 @@
 - [x] Dozen bet
 - [x] Column bet
 - [x] Split bet (2number 17:1)
-- [ ] Street bet (3number 11:1)
+- [x] Street bet (3number 11:1)
 - [ ] Corner bet (4number 8:1)
 - [ ] Line bet (6number 5:1)
--- src/com/ktung/Main.java
+++ src/com/ktung/Main.java
@@ -3,14 +3,15 @@ package com.ktung;
 import com.ktung.game.bet.Black;
 import com.ktung.game.bet.Red;
 import com.ktung.game.bet.Split;
+import com.ktung.game.bet.Street;
 import com.ktung.simulator.Simulator;
 
 public class Main {
 
     public static void main(String[] args) {
         Simulator simulator = new Simulator();
-        simulator.addBet(new Split(1, 1,2));
-        simulator.addBet(new Split(10, 1, 5));
+        simulator.addBet(new Street(1, 1,2, 3));
+        simulator.addBet(new Street(10, 33, 32, 31));
 
         simulator.showOdds();
         int pnl = simulator.run(10);
