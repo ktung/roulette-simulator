9ec3694610f5aa61b260f103b54793a0cef8c14b
ptung <misuke01@gmail.com>
2021-12-14 13:56:43 -0500

    Add odd

---
+++ src/com/ktung/game/Odd.java
@@ -0,0 +1,15 @@
+package com.ktung.game;
+
+public class Odd extends Bet {
+
+  public Odd(int bet) {
+    init();
+    this.bet = bet;
+  }
+
+  private void init() {
+    this.winningNumbers = new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35};
+    this.payout = 1;
+  }
+
+}
