8b3385a3ff962a3d83574e0320eab29393d5896b
ptung <misuke01@gmail.com>
2021-12-16 18:28:03 -0500

    Test scenario add safe bet

--- src/main/java/com/ktung/Main.java
+++ src/main/java/com/ktung/Main.java
@@ -9,17 +9,31 @@ public class Main {
 
     public static void main(String[] args) {
         Simulator simulator = new Simulator();
-        simulator.addBet(new Red(100));
-        simulator.addBet(new Corner(1, 0, 1, 2, 3));
 
-        simulator.showOdds();
-        int pnl = simulator.run(10);
-        if (pnl > 0) {
-            System.out.println("Yay!");
-        } else if (pnl == 0) {
-            System.out.println("Yay...");
-        } else {
-            System.out.println("...");
+        int initialBalance = 200;
+        int miseTotale = 0;
+
+        int balance = initialBalance;
+        int nbIteration = 10;
+        int totalWin = 0;
+        int pnlPerRound = 0;
+        for (int i = 0; i < nbIteration; i++) {
+            if (pnlPerRound <= 0) {
+                miseTotale += balance;
+            }
+
+            System.out.printf("%d | Initial balance %d%n", i, balance);
+            pnlPerRound = simulator.run2(balance, 40, 10, 1.8);
+            totalWin += pnlPerRound;
+
+            if (pnlPerRound > 0) {
+                balance += pnlPerRound/2;
+            } else {
+                balance = initialBalance+pnlPerRound;
+            }
         }
+
+        System.out.printf("Mise totale %d%n", miseTotale);
+        System.out.printf("totalWin %d (%f)", totalWin, (((double)totalWin/miseTotale)*100));
     }
 }
--- src/main/java/com/ktung/simulator/Simulator.java
+++ src/main/java/com/ktung/simulator/Simulator.java
@@ -1,5 +1,6 @@
 package com.ktung.simulator;
 
+import com.ktung.game.bet.Red;
 import com.ktung.game.bet.abs.Bet;
 import com.ktung.game.Constants;
 
@@ -8,6 +9,10 @@ import java.util.List;
 import java.util.Random;
 
 public class Simulator {
+
+  private final Random random = new Random();
+  private List<String> outputs = new ArrayList<>();
+
 
   private final List<Bet> bets = new ArrayList<>();
 
@@ -20,13 +25,11 @@ public class Simulator {
   }
 
   public int run(int nbPlay) {
-    List<String> outputs = new ArrayList<>();
-
+    outputs.clear();
     int sumBet = bets.stream().map(Bet::getBet).reduce(0, Integer::sum);
     int totalBet = sumBet*nbPlay;
     int totalWon = 0;
 
-    Random random = new Random();
     for (int i = 0; i < nbPlay; i++) {
       int result = random.nextInt(Constants.NB_POSSIBILITIES);
 
@@ -37,6 +40,64 @@ public class Simulator {
     int pnl = totalWon-totalBet;
     outputs.add(String.format("Bet : %d, Won : %d => P/L %d%n", totalBet, totalWon, pnl));
     System.out.println(outputs);
+
+    return pnl;
+  }
+
+  public int run2(int balance, int maxRound, int initialBet, double limitWin) {
+    boolean verbose = true;
+    outputs.clear();
+    int initialBalance = balance;
+    int savings = 0;
+
+    bets.clear();
+    this.addBet(new Red(initialBet));
+
+    for (int i = 0; i < maxRound && balance > 0; i++) {
+      int sumBet = bets.stream().map(Bet::getBet).reduce(0, Integer::sum);
+
+      int result = random.nextInt(Constants.NB_POSSIBILITIES);
+//      int result = 1;
+      int won = bets.stream().map(bet -> bet.calculateWin(result)).reduce(0, Integer::sum);
+      balance += (won-sumBet);
+      outputs.add(String.format("%d | Result : %d, Bet : %d, Won : %d | Balance %d%n", i, result, sumBet, won, balance));
+
+      if (balance >= initialBalance*limitWin) {
+        break;
+      }
+
+      if (won > 0) {
+//        this.addBet(new Red(5));
+        int saving = initialBet/2;
+        if (savings >= 2*initialBalance) {
+          savings /= 2;
+        }
+        savings += saving;
+        int safeBet = won/2-savings;
+        if (sumBet+safeBet <= 0) {
+          if (verbose) {
+            System.out.printf("No more safe bet %d%n", sumBet+safeBet);
+          }
+          break;
+        }
+        this.addBet(new Red(won/2-savings));
+      } else {
+        bets.clear();
+        if ((balance-savings) >= initialBet) {
+          this.addBet(new Red(initialBet));
+        } else {
+          break;
+//          this.addBet(new Red(balance));
+        }
+      }
+    }
+
+    int pnl = balance-initialBalance;
+    double pnlPercent = ((double)balance/initialBalance -1)*100;
+    outputs.add(String.format("Bet : %d, Won : %d => P/L %d (%f%%) [savings %d]%n", initialBalance, balance, pnl, pnlPercent, savings));
+    if (verbose) {
+      System.out.println(outputs);
+    }
 
     return pnl;
   }
